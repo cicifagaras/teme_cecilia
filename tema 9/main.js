15c81d0892e91a52beab4fee3541c411e2ad32cb
cicifagaras <cicifagaras@yahoo.ro>
2022-04-16 22:01:32 +0300

    Shopping List commit

---
+++ tema 9/main.js
@@ -0,0 +1,64 @@
+let state = {
+    list: [],
+    checked: []
+}
+
+function draw() {
+    let table = document.querySelector("#list tbody");
+    let string = "";
+
+    for (let i = 0; i < state.list.length; i++) {
+        let element = state.list[i];
+        let items = document.querySelectorAll(".items");
+
+        if (state.checked[i]) {
+            string += `
+                            <tr>
+                                <td class="items marked">${element}</td>
+                                <td><button class="markButtons" onclick="markAsBuyed(${i})">Mark as buyed</td>
+                            </tr>
+                    `;
+        }
+        else{
+            string += `
+                    <tr>
+                        <td class="items">${element}</td>
+                        <td><button class="markButtons" onclick="markAsBuyed(${i})">Mark as buyed</td>
+                    </tr>
+                `;
+        }
+
+    }
+    table.innerHTML = string;
+}
+
+
+function addItem(event) {
+    event.preventDefault();
+    let item = document.querySelector(`[name = "item"]`).value;
+
+    if (item !== "") {
+        state.list.push(item);
+        state.checked.push(false);
+    }
+    draw();
+}
+
+
+function markAsBuyed(index) {
+    let items = document.querySelectorAll(".items");
+    items[index].classList.add("marked");
+    state.checked[index] = true;
+}
+
+function sortAsc() {
+    state.list.sort();
+    draw();
+}
+
+
+function sortDesc() {
+    state.list.sort();
+    state.list.reverse();
+    draw();
+}
